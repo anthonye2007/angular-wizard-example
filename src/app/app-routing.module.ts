97388d84fd8c6bb020cc96ecd3cecf1798baee34
Anthony Elliott <aelliott@pillartechnology.com>
2018-10-16 17:14:22 -0500

    Tell tests about router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [
-    RouterModule
-  ]
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }

75a8ff2cbe9e7a0052aa168089443643c64d99be
Anthony Elliott <aelliott@pillartechnology.com>
2018-10-18 13:48:44 -0500

    Add script to run e2e tests inside docker container

---
+++ run-e2e-tests.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+docker pull anthonye2007/docker-protractor
+
+docker run -it \
+ --privileged \
+ --net=host \
+ --rm
+ --name protractor-test \
+ -v $(PWD):/protractor/project \
+  anthonye2007/docker-protractor yarn e2e

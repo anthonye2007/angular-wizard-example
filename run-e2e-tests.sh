c851d979168a9f969434e4fa1acf36649dafff81
Anthony Elliott <aelliott@pillartechnology.com>
2018-10-18 14:18:14 -0500

    Fix docker run cmd

--- run-e2e-tests.sh
+++ run-e2e-tests.sh
@@ -5,7 +5,7 @@ docker pull anthonye2007/docker-protractor
 docker run -it \
  --privileged \
  --net=host \
- --rm
+ --rm \
  --name protractor-test \
  -v $(PWD):/protractor/project \
   anthonye2007/docker-protractor yarn e2e
